3018748bd4bc568e677cec95cfb379525c4b18a0
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2021-02-22 19:25:09 +0800

    Add entities

---
+++ app/WebService.Entities/Context/CommandDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebService.Entities.Context
+{
+    public partial class CommandDBContext : DbContext
+    {
+        public CommandDBContext(DbContextOptions<CommandDBContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ app/WebService.Entities/Context/QueryDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebService.Entities.Context
+{
+    public partial class QueryDBContext : DbContext
+    {
+        public QueryDBContext(DbContextOptions<QueryDBContext> options)
+            : base(options)
+        {
+        }
+    }
+}
